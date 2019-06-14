2b5593be590a598772d71263f255d3a202909742
anryan <thoughtworks.shao@daimler.com>
2019-06-14 11:23:38 +0800

    template

---
+++ template_hello.py
@@ -0,0 +1,14 @@
+
+from flask import Flask
+from flask import render_template
+
+app=Flask(__name__)
+
+@app.route('/sayhi')
+@app.route('/sayhi/<name>')
+def index(name=None):
+    return render_template('hello.html',name=name)
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',debug=True)
+
