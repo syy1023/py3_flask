612f95c47db1024ef9af0d783d67a99e1c5a81bb
anryan <thoughtworks.shao@daimler.com>
2019-06-15 14:43:17 +0800

    template inherit example

---
+++ template_inherite_parent.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+@app.route('/sayhello/<location>')
+@app.route('/sayhi/<name>')
+def index(name=None,location=None):
+    return render_template('child1.html',name=name,location=location)
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',debug=True)
